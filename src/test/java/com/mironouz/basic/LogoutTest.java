8831941a2c659d5a20390987b730e015e851dd78
Dmitrii Mironov <mironouz@gmail.com>
2017-06-07 16:49:12 +0200

    Added fluent wait into logout test

--- src/test/java/com/mironouz/basic/LogoutTest.java
+++ src/test/java/com/mironouz/basic/LogoutTest.java
@@ -5,14 +5,19 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.Properties;
+import java.util.function.Function;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static junit.framework.TestCase.assertTrue;
 
 /**
@@ -35,8 +40,18 @@ public class LogoutTest {
     @Test
     public void testLogout(){
         Utility.login(correctLogin, correctPassword, driver);
-        WebElement smile = new WebDriverWait(driver, 10)
-                .until(ExpectedConditions.visibilityOfElementLocated(By.className("mc-avatar-image")));
+
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+                .withTimeout(30, SECONDS)
+                .pollingEvery(1, SECONDS)
+                .ignoring(NoSuchElementException.class);
+
+        WebElement smile = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(By.className("mc-avatar-image"));
+            }
+        });
+        
         smile.click();
         driver.findElement(By.xpath("//a[@href='https://www.dropbox.com/logout']")).click();
         assertTrue(driver.getCurrentUrl().endsWith("logout"));
